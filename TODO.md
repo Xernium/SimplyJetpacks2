d83da1adefe8e97659d5945960edd02d99d0afef
Five (Xer) <admin@fivepb.me>
2024-09-04 00:16:34 +0200

    Fix creative mode tab

--- TODO.md
+++ TODO.md
@@ -9,10 +9,11 @@
 
 - [x] Fix advancements entirely
 - [x] Fix Potato jetpack explosions
-- [ ] Fix creative mode tab crash (shadow renderer error)
+- [x] Fix creative mode tab crash (shadow renderer error)
 - [x] Make player model in menu work again correctly (follow cursor)
 - [x] Fix Armor toughness values ....
 - [x] Fix crash caused by swapping jetpack from inventory slot
 - [x] Fix or replace the onGround tick accessor for multiplayer
 - [x] Fix item tag data gen
 - [x] Fix weird ghost state when using right click to equip jetpack
+- [ ] Check advancement triggers
--- src/main/java/stormedpanda/simplyjetpacks/item/SJItemGroup.java
+++ src/main/java/stormedpanda/simplyjetpacks/item/SJItemGroup.java
@@ -1,6 +1,7 @@
 package stormedpanda.simplyjetpacks.item;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +19,8 @@ public class SJItemGroup extends CreativeModeTab {
         super(CreativeModeTab.builder()
                 .title(Component.translatable("itemGroup." + SimplyJetpacks.MODID + ".main"))
                 .icon(()-> new ItemStack(RegistryHandler.JETPACK_CREATIVE.get()))
+                // I have *zero* clue why this is required for this mod _exclusively_ but I don't care enough to figure out, why.
+                .withBackgroundLocation(new ResourceLocation("minecraft", "textures/gui/container/creative_inventory/tab_items.png"))
                 .displayItems((params, output) -> {
                     for(RegistryObject<Item> i : RegistryHandler.ITEMS.getEntries()) {
                         Item item = i.get();
